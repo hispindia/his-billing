6606057702957830c7d972b224f31fea1bc9390b
Bob Jolliffe <bobj@bobj-laptop.(none)>
2011-10-06 23:27:05 +0100

    Replaced headers to all java files

--- omod/src/main/java/org/openmrs/module/billing/web/controller/main/TenderBillListController.java
+++ omod/src/main/java/org/openmrs/module/billing/web/controller/main/TenderBillListController.java
@@ -1,16 +1,23 @@
 /**
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
+ *  Copyright 2011 Health Information Systems Project of India
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ *  This file is part of Billing module.
  *
- * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
- */
+ *  Billing module is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  Billing module is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Billing module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
 package org.openmrs.module.billing.web.controller.main;
 
 import javax.servlet.http.HttpServletRequest;
