4ce805dc605218cdefc492e65660143365be3926
hiepkhachbk <thaichuong159@gmail.com>
2012-05-11 06:45:58 -0700

    - Fixed bug#188 (TESTING RESULT GIT_HIMACHAL PRADESH project) - Update the version for billing to 3.2.4 (This version also contain the feature for 3.2.4 implement for mohali on 05/05/2012 & have more features)

--- api/src/main/java/org/openmrs/module/billing/includable/billcalculator/solan/BillCalculatorImpl.java
+++ api/src/main/java/org/openmrs/module/billing/includable/billcalculator/solan/BillCalculatorImpl.java
@@ -33,53 +33,54 @@ import org.openmrs.module.hospitalcore.concept.TestTree;
 import org.openmrs.module.hospitalcore.model.PatientServiceBillItem;
 
 public class BillCalculatorImpl implements BillCalculator {
-
+	
 	private static Map<String, Set<Concept>> testTreeMap;
-
+	
 	/**
-	 * Get the percentage of price to pay If patient category is RSBY or BPL,
-	 * the bill should be 100% free
+	 * Get the percentage of price to pay If patient category is RSBY or BPL, the bill should be
+	 * 100% free
 	 * 
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
 	public BigDecimal getRate(Map<String, Object> parameters) {
 		BigDecimal rate = new BigDecimal(1);
-		Map<String, String> attributes = (Map<String, String>) parameters
-				.get("attributes");
+		Map<String, String> attributes = (Map<String, String>) parameters.get("attributes");
 		String patientCategory = attributes.get("Patient Category");
-		PatientServiceBillItem item = (PatientServiceBillItem) parameters
-				.get("billItem");		
-
+		PatientServiceBillItem item = (PatientServiceBillItem) parameters.get("billItem");
+		
 		if (!StringUtils.isBlank(patientCategory)) {
-
+			
 			if (patientCategory.contains("BPL")) {
-
 				return new BigDecimal(0);
 			} else if (patientCategory.contains("RSBY")) {
-
 				return new BigDecimal(0);
-			} else if (patientCategory.contains("Senior Citizen")) {
-
+			} else if (patientCategory.contains("Antenatal")) {
+				return new BigDecimal(0);
+			} else if (patientCategory.contains("Child Less Than 1yr")) {
+				return new BigDecimal(0);
+			} else if (patientCategory.contains("Other Free")) {
+				return new BigDecimal(0);
+			}
+			else if (patientCategory.contains("Senior Citizen")) {
+				
 				// Get test tree map
 				if (testTreeMap == null) {
 					buildTestTreeMap();
 				}
 				System.out.println("Billing test tree" + testTreeMap);
-
+				
 				// CALCULATE THE RATE
-				Concept test = Context.getConceptService().getConcept(
-						item.getService().getConceptId());
-
+				Concept test = Context.getConceptService().getConcept(item.getService().getConceptId());
+				
 				if (testTreeMap.get("GENERAL LABORATORY").contains(test)) {
-
+					
 					// GENERAL LABORATORY
-					if (!test.getName().getName()
-							.equalsIgnoreCase("LIPID PANEL")) {
+					if (!test.getName().getName().equalsIgnoreCase("LIPID PANEL")) {
 						return new BigDecimal(0);
 					}
 				} else if (testTreeMap.get("RADIOLOGY").contains(test)) {
-
+					
 					// RADIOLOGY and ULTRASOUND
 					if (testTreeMap.get("ULTRASOUND").contains(test)) {
 						return new BigDecimal(0);
@@ -87,45 +88,50 @@ public class BillCalculatorImpl implements BillCalculator {
 				}
 			}
 		}
-
+		
 		return rate;
 	}
-
+	
 	/**
 	 * Build test tree map for senior citizen billing
 	 */
 	private static void buildTestTreeMap() {
 		testTreeMap = new HashMap<String, Set<Concept>>();
-
+		
 		// General lab
 		buildTestTree("GENERAL LABORATORY");
 		buildTestTree("RADIOLOGY");
 		buildTestTree("ULTRASOUND");
 		buildTestTree("CARDIOLOGY");
 	}
-
+	
 	/**
 	 * Build test tree for a specific tests
 	 * 
 	 * @param conceptName
 	 */
 	private static void buildTestTree(String conceptName) {
-		Concept generalLab = Context.getConceptService()
-				.getConcept(conceptName);
+		Concept generalLab = Context.getConceptService().getConcept(conceptName);
 		TestTree tree = new TestTree(generalLab);
 		if (tree.getRootNode() != null) {
 			testTreeMap.put(conceptName, tree.getConceptSet());
 		}
 	}
-
+	
 	/**
-	 * Determine whether a bill should be free or not. By default, all bills are
-	 * not free
+	 * Determine whether a bill should be free or not. By default, all bills are not free
 	 * 
 	 * @return
 	 */
 	public boolean isFreeBill(Map<String, Object> parameters) {
-
-		return false;
+		Map<String, String> attributes = (Map<String, String>) parameters.get("attributes");
+		String patientCategory = attributes.get("Patient Category");
+		
+		// 11/05/12 - Thai Chuong, chaged to consider the new free categories. Fixed bug#188
+		if (patientCategory.contains("General")) {
+			return false;
+		}
+		
+		return true;
 	}
 }
